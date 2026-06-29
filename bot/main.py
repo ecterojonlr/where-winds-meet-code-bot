24d53e25e9a23c2e80f3ab46d0967ebd8fd86a9f
ecterojonlr <ecterojonlr@gmail.com>
2026-06-30 02:53:19 +0800

    Add main function to run Discord bot

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,21 @@
+import asyncio
+import os
+from datetime import datetime
 
+from bot.discord_bot import DiscordBot
+
+TOKEN = os.environ["DISCORD_TOKEN"]
+CHANNEL_ID = int(os.environ["CHANNEL_ID"])
+
+
+async def main():
+    bot = DiscordBot(TOKEN, CHANNEL_ID)
+
+    await bot.send_codes(
+        ["TEST123"],
+        datetime.now().strftime("%Y/%m/%d"),
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
